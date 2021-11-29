ec0c5f84d8d6cd7845ac4c228191624107edf1ca
AqsaBano330 <64906746+AqsaBano330@users.noreply.github.com>
2021-11-29 10:25:25 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("hi guys");
+            Console.WriteLine("bye ladies");
+        }
+    }
+}
